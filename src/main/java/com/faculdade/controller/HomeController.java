d34867245eb10ffca16d1f88c1c9b5ca40da00cb
DiegoRodrigues06 <diegodash06@gmail.com>
2025-10-28 10:32:42 -0300

    first commit

---
+++ src/main/java/com/faculdade/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.faculdade.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // renderiza templates/index.html
+    }
+}
